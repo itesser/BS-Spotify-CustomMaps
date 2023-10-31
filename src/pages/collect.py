daf60cfc53c0816c68ac670a90717164f28a4a70
itesser <itesser@gmail.com>
2023-10-31 09:42:24 -0700

    organized files and working on error handling

--- src/pages/collect.py
+++ src/pages/collect.py
@@ -8,21 +8,35 @@ from spotpull import SpotPull
 from to_mongo import ToMongo
 
 st.title("Collect Some Songs")
-st.text("enter a date to check 50 random songs posted on that date")
-st.text("empty or invalid dates will return the most recent 50 songs")
+st.text("Enter a date to check 50 random songs posted on that date")
+st.text("Empty or invalid dates will return the most recent 50 songs")
 
 st.header("Please use YYYY-MM-DD format")
 st.text("results may take 60 seconds to appear")
 date = st.text_input("Enter date here")
 if st.button("Get Songs"):
     s = SpotPull(date)
-    s.lookit_me = s.bs_data[s.bs_data['match_conclusion']]
-    st.dataframe(s.lookit_me[['title', 'artist', 'mapper_name', 'difficulties','bs_map_link','spotify_link']])
+    s.lookit_me = s.bs_data[s.bs_data["match_conclusion"]]
+    st.dataframe(
+        s.lookit_me[
+            [
+                "title",
+                "artist",
+                "mapper_name",
+                "difficulties",
+                "bs_map_link",
+                "spotify_link",
+            ]
+        ]
+    )
     st.subheader("What's next?")
     s.local_save()
-    st.text('local csv ready!')
+    st.text("Local CSV ready! - Go to the Analyze page to see stats on these songs")
     tm = ToMongo()
     tm.update_stats()
     tm.upload_one_by_one(s.bs_data)
-    st.text('mongo upload done!')
+    st.text(
+        "Mongo upload done! - These songs will be incorporated in Stats - All Time and Query results"
+    )
     tm.client.close()
+    st.text
