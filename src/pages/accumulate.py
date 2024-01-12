1e962237713cec38f9a7eea0ef6e42c62c701f68
itesser <itesser@gmail.com>
2024-01-12 13:41:09 -0800

    Updating for error-checking
    accumulate page works locally but not on web

--- src/pages/accumulate.py
+++ src/pages/accumulate.py
@@ -22,42 +22,44 @@ minute = st.slider("Minute", min_value=-1, max_value=59)
 date = str(date)
 if st.button("Get Those Songs"):
     st.write("Please be patient, results may take 60 seconds to appear")
-    try:
-        s = SpotPull(date, hour, minute)
-        s.lookit_me = s.bs_data
-        st.caption(
-            """Buttons in the upper-right corner of the table can be used to:
-            Download as CSV, Search, or View Full Screen"""
-        )
+    s = SpotPull(date, hour, minute)
+    s.lookit_me = s.bs_data
+    st.caption(
+        """Buttons in the upper-right corner of the table can be used to:
+        Download as CSV, Search, or View Full Screen"""
+    )
 
-        st.dataframe(
-            s.lookit_me[
-                [
-                    "match_conclusion",
-                    "title",
-                    "artist",
-                    "mapper_name",
-                    "difficulties",
-                    "bs_map_link",
-                    "spotify_link",
-                ]
-            ].set_index(s.lookit_me.columns[40])
-        )
-        st.subheader("What's next?")
-        st.text("Wait just a bit longer for me to digest this data...")
-        s.local_save()
-        st.write(
-            "Local CSV ready! - Go to the Analyze page to see stats on these songs"
-        )
-        st.text("...adding songs to database...")
+    st.dataframe(
+        s.lookit_me[
+            [
+                "match_conclusion",
+                "title",
+                "artist",
+                "mapper_name",
+                "difficulties",
+                "bs_map_link",
+                "spotify_link",
+            ]
+        ].set_index(s.lookit_me.columns[40])
+    )
+    st.subheader("What's next?")
+    st.text("Wait just a bit longer for me to digest this data...")
+    s.local_save()
+    st.write("Local CSV ready! - Go to the Analyze page to see stats on these songs")
+    st.text("...adding songs to database...")
+    try:
         tm = ToMongo()
+    except:
+        st.write("error 1")
+    try:
         tm.update_stats()
+    except:
+        st.write("error 2")
+    try:
         tm.upload_one_by_one(s.bs_data)
         st.write(
             "Mongo upload done! - These songs will be incorporated in Stats - All Time and Query results"
         )
     except:
-        st.write(
-            "Unable to find songs on that date. Please select a new date and try again"
-        )
+        st.write("error 3")
         st.write("If the problem persists, try reloading this webpage.")
